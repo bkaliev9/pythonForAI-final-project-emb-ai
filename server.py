9687507c10b85807e687aea1ea7b80af3a6d737d
Bulat Kaliev <bkaliev@me.com>
2024-08-27 17:54:19 -0400

    updated based on pylint

--- server.py
+++ server.py
@@ -1,24 +1,31 @@
-from flask import Flask, request, jsonify, render_template
-from EmotionDetection import emotion_detector
+"""
+Emotion detection server
+"""
 import json
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    index page
+    """
     return render_template('index.html')
 
 @app.route('/emotionDetector')
-def emotionDetector():
+def detect_emotion():
+    """
+    emotion detection function
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
-    return result
     result_dic = json.loads(result)
 
     if result_dic['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    else:
-        return result
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
